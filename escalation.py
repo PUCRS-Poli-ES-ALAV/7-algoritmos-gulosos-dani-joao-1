74b45c2bdfaca6960e73ffebdca3f53352675919
Daniela Pereira Rigoli <Daniela.Rigoli@edu.pucrs.br>
2022-04-04 20:39:55 -0300

    exchange problem almost solved

--- escalation.py
+++ escalation.py
@@ -14,16 +14,19 @@ def escalation2(s,f,n):
     return x
 """
 
-def escalation3(s, f, n):
-    f[0] = Decimal('-Infinity')
-    x = []
-    i = 0
+def escalation3(sf):
+    n = len(sf)
+    sf = list(sorted(sf.items(), key=lambda item: item[1]))
 
-    for k in range(1,n):
-        if s[k] > f[i]:
-            x = x + [k]
+    X = []
+    i = 0
+    for k in range(n):
+        if sf[k][0] > sf[i][1]:
+            X.append(sf[k])
             i = k
-    return x
+    
+    return X
+    
 
 #print(escalation2([4,6,13,4,2,6,7,9,1,3,9], [8,7,14,5,4,9,10,11,6,13,12],4))
-print(escalation3([4,6,13,4,2,6,7,9,1,3,9], [8,7,14,5,4,9,10,11,6,13,12],10))
+print(escalation3({4:8,6:7,13:14,4:5,2:4,6:9,7:10,9:11,1:6,3:13,9:12}))
